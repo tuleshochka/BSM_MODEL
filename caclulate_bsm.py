768c8a3b2c8f3cd41d33e070a571935472a06c48
tuleshochka <sizyno.min@gmail.com>
2023-10-01 10:59:24 +0600

    !

--- caclulate_bsm.py
+++ caclulate_bsm.py
@@ -4,7 +4,7 @@ import pickle
 import numpy as np
 import os
 
-print("Please enter the path to file containing data")
+print("Please enter the path to file containing data about flight schedule")
 path_to_dataset = input()
 
 try:
@@ -52,8 +52,64 @@ dataset['m_city_rus1'] = le.transform(dataset['m_city_rus1'])
 le.fit(dataset['m_city_rus2'])
 dataset['m_city_rus2'] = le.transform(dataset['m_city_rus2'])
 
+print("Please enter the path to file containing data about history of BSM")
+path_to_dataset_2 = input()
+
+import csv
+
+import codecs
+
+try:
+    dataset_2 = pd.read_csv(path_to_dataset_2)
+except:
+    print("There was an error reading the file. Maybe, you have provided the wrong path")
+    exit()
+
+listA = []
+with codecs.open(path_to_dataset, 'r', "utf_8_sig") as file:
+    reader = csv.reader(file)
+    for line in reader:
+        print(line[0])
+        if line[0] != 'i_id':
+            listA.append(line[0])
+
+listB = []
+with codecs.open(path_to_dataset_2, 'r', "utf_8_sig") as file:
+    reader = csv.reader(file)
+    for line in reader:
+        if line[0] != 'ident_bag':
+            listB.append(line[1])
+
+listC = []
+for i in range(len(listA)):
+    sum = 0
+    for j in range(len(listB)):
+        print(listA[i])
+        print(listB[j])
+        if listA[i] == listB[j]:
+            sum += 1
+    listC.append(sum)
+
+print(listC)
+
+dict = {'history_bsm': listC}
+df = pd.DataFrame(dict)
+df.to_csv('BSM.csv', index=False)
+# path = 'BSM.csv'
+# with codecs.open(path, 'r', "utf_8_sig") as file:
+#     writer = csv.writer(file)
+#     for i in range(len(listC)):
+#         writer.writerow([listC[i]])
+
+
+BSM = pd.read_csv('BSM.csv')
+
+dataset['history_bsm'] = BSM
+
+print(dataset.head())
+
 # load the model from disk
-loaded_model = pickle.load(open('RNN_model_200.sav', 'rb'))
+loaded_model = pickle.load(open('LR_model.sav', 'rb'))
 
 prediction = loaded_model.predict(dataset)
 prediction = (np.ceil(prediction)).astype(int)
